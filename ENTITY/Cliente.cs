397ea64e38d0d021d8a16bd394d64be25ec17ae8
German Ballesteros <168650066+GermanBallesterosT@users.noreply.github.com>
2024-05-02 09:27:58 -0500

    Update Cliente.cs
    ya quedo la clase cliente

--- ENTITY/Cliente.cs
+++ ENTITY/Cliente.cs
@@ -15,5 +15,40 @@ namespace ENTITY
         {
             CodigoCliente = codigoCliente;
         }
+
+         public Cliente crearNuevoCliente()
+         {
+             Cliente cliente = new Cliente();
+             Persona persona = new Persona();
+             string codigo;
+        
+             Console.SetCursorPosition(20, 5); Console.Write("Registrar Nuevo Cliente");
+             persona = persona.crearNuevaPersona();
+             cliente.id = persona.id;
+             cliente.nombre = persona.nombre;
+             cliente.apellido = persona.apellido;
+             cliente.telefono = persona.telefono;
+             cliente.direccion = persona.direccion;
+        
+             while (true)
+             {
+                 Console.SetCursorPosition(10, 17); Console.Write("                                                     ");
+        
+                 Console.SetCursorPosition(10, 12); Console.Write("Asigne Codigo Al Cliente: ");
+                 Console.SetCursorPosition(50, 12); codigo = Console.ReadLine();
+                 if (!String.IsNullOrEmpty(codigo))
+                 {
+                     cliente.CodigoCliente = codigo;
+                     break;
+                 }
+                 else
+                 {
+                     Console.SetCursorPosition(10, 17); Console.Write("Error: No Se Admiten Campos Vacios");
+                     Console.ReadKey();
+                 }
+             }
+        
+             return cliente;
+         }
     }
 }

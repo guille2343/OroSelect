4f9a37b3484b4530cd2d4c061e02846b6f89e954
German Ballesteros <168650066+GermanBallesterosT@users.noreply.github.com>
2024-05-02 19:19:00 -0500

    Las funciones agregadas en la entidad  inventario contrato aun estan para revision

--- ENTITY/InventarioContrato.cs
+++ ENTITY/InventarioContrato.cs
@@ -13,6 +13,34 @@ namespace ENTITY
         public List<ContratoVenta> contratosVenta = new List<ContratoVenta>();
 
         public InventarioContrato() { }
+
+          public void AlmacenarContrato<T>(T contrato) 
+          {
+              if (contrato is ContratoCompra)
+                  contratosCompra.Add(contrato as ContratoCompra);
+              else if (contrato is ContratoCompromiso)
+                  contratosCompromiso.Add(contrato as ContratoCompromiso);
+              else if (contrato is ContratoVenta)
+                  contratosVenta.Add(contrato as ContratoVenta);
+          }
+        
+          public T buscarContratoEnLista <T>(List<T> listaContratos, string codigoContrato)
+          {
+              return listaContratos.FirstOrDefault();
+          }
+        
+          //public T BuscarContratoPorCodigo<T>(List<T> listaContratos, string codigoContrato)
+          //{
+          //    foreach (var contrato in listaContratos)
+          //    {
+          //        if (contrato.Codigo == codigoContrato)
+          //        {
+          //            return contrato;
+          //        }
+          //    }
+          //    return null;
+          //}   TODAVIA FALTA LA FUNCION PARA RECORRESR LAS LISTAS EN BUSCA DE UN OBJETO
+
        
     }
 }
